cb43e8d00711cb6231f402dbb3cd920c45d2e6bb
nurfadilah azzahra <nurfadilzr@gmail.com>
2023-12-04 16:14:59 +0800

    fix: add features

--- resources/views/homep.blade.php
+++ resources/views/homep.blade.php
@@ -5,52 +5,56 @@
 @extends('partials/navbar')
 
 <style>
-    .carousel-caption {
-        color: black;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        color: #393939;
-        
-    }
+   .carousel-caption {
+      color: black;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: #393939;
+      
+   }
 
-    h5 {
-        font-family: poppins;
-        font-weight: 700;
-        font-size: 28px;
-        margin-top: 50px;
-        white-space: nowrap;
-    }
-    p {
-        font-family: poppins;
-        font-weight: 400;
-        font-size: 20px;
-    }
-    .btn-our {
-        background-color: #019267;
-        color: #fff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 20px 0;
-        padding: 10px 0;
-        font-family: poppins;
-        font-weight: 600;
-        font-size: 24px;
-        text-align: center;
-        text-decoration: none;
-        border-radius: 10px;
-    } 
-    .btn-our:hover {
-        color: #d1d1d1;
-        text-decoration-color: none;
-    }
+   h5 {
+      font-family: poppins;
+      font-weight: 700;
+      font-size: 28px;
+      margin-top: 50px;
+      white-space: nowrap;
+   }
+   p {
+      font-family: poppins;
+      font-weight: 400;
+      font-size: 20px;
+   }
+   .card-title {
+   font-family: poppins;
+   font-weight: 600;
+   font-size: 20px;
+   }
+   .btn-our {
+      background-color: #019267;
+      color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 20px 0;
+      padding: 10px 0;
+      font-family: poppins;
+      font-weight: 600;
+      font-size: 24px;
+      text-align: center;
+      text-decoration: none;
+      border-radius: 10px;
+   } 
+   .btn-our:hover {
+      color: #d1d1d1;
+      text-decoration-color: none;
+   }
     
-    .carousel-inner {
-        margin-top: 50px;
-    }
-
+   .carousel-inner {
+      margin-top: 50px;
+   }
 
 </style>
 
@@ -66,87 +70,121 @@
 </div>
 
 <div class="container">
-    <section class="products">
-        
-        <a type="button" href="" class="d-grid link-offset-2 link-underline-opacity-25 link-underline-opacity-25-hover btn-our" data-bs-toggle="button">Our Products</a>
+   <section id="features" class="features">
+      
+      <a type="button" href="" class="d-grid link-offset-2 link-underline-opacity-25 link-underline-opacity-25-hover btn-our" data-bs-toggle="button">Our Features</a>
 
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
-              <div class="card h-50">
-                <img src="img/carrot.jpg" class="card-img-top" alt="vegies-carrot">
-                <div class="card-body">
-                  <h5 class="card-title">Carrot</h5>
-                  <p class="card-text">This</p>
-                </div>
-              </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+         <div class="col">
+            <div class="card h-50">
+               <img src="img/cart.jpg" class="card-img-top" alt="vegies-carrot">
+               <div class="card-body">
+               <h5 class="card-title">Keranjang Belanjang</h5>
+               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+               </div>
             </div>
-            <div class="col">
-              <div class="card h-50">
-                <img src="img/garlic.jpg" class="card-img-top" alt="vegies-garlic">
-                <div class="card-body">
-                  <h5 class="card-title">Garlic</h5>
-                  <p class="card-text">This</p>
-                </div>
-              </div>
+         </div>
+         <div class="col">
+            <div class="card h-50">
+               <img src="img/lup.jpg" class="card-img-top" alt="vegies-garlic">
+               <div class="card-body">
+               <h5 class="card-title">Search Bar</h5>
+               <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
             </div>
-            <div class="col">
-              <div class="card h-50">
-                <img src="img/lettuce.jpg" class="card-img-top" alt="vegies-lettuce">
-                <div class="card-body">
-                  <h5 class="card-title">Lettuce</h5>
-                  <p class="card-text">This</p>
-                </div>
-              </div>
+         </div>
+         <div class="col">
+            <div class="card h-50">
+               <img src="img/best.jpg" class="card-img-top" alt="vegies-lettuce">
+               <div class="card-body">
+               <h5 class="card-title">Produk Berkualitas</h5>
+               <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
             </div>
-            <div class="col">
-              <div class="card h-50">
-                <img src="img/tomato.jpg" class="card-img-top" alt="vegies-tomato">
-                <div class="card-body">
-                  <h5 class="card-title">Tomato</h5>
-                  <p class="card-text">This</p>
-                </div>
-              </div>
-            </div>
-        </div>
+         </div>
+      </div>
+   </section>
 
-        <div class="d-md-flex justify-content-md-end">
-            <a href="" class="btn btn-outline-dark" >See more...</a>
-        </div>
-    </section>
+   <section id="product" class="product">
+      
+      <a type="button" href="" class="d-grid link-offset-2 link-underline-opacity-25 link-underline-opacity-25-hover btn-our" data-bs-toggle="button">Our Products</a>
 
-    <section class="categories">
-        <a type="button" href="" class="d-grid btn-our" data-bs-toggle="button">Our Categories</a>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+         <div class="col">
+            <div class="card h-50">
+               <img src="img/carrot.jpg" class="card-img-top" alt="vegies-carrot">
+               <div class="card-body">
+               <h5 class="card-title">Carrot</h5>
+               <p class="card-text">This</p>
+               </div>
+            </div>
+         </div>
+         <div class="col">
+            <div class="card h-50">
+               <img src="img/garlic.jpg" class="card-img-top" alt="vegies-garlic">
+               <div class="card-body">
+               <h5 class="card-title">Garlic</h5>
+               <p class="card-text">This</p>
+               </div>
+            </div>
+         </div>
+         <div class="col">
+            <div class="card h-50">
+               <img src="img/lettuce.jpg" class="card-img-top" alt="vegies-lettuce">
+               <div class="card-body">
+               <h5 class="card-title">Lettuce</h5>
+               <p class="card-text">This</p>
+               </div>
+            </div>
+         </div>
+         <div class="col">
+            <div class="card h-50">
+               <img src="img/tomato.jpg" class="card-img-top" alt="vegies-tomato">
+               <div class="card-body">
+               <h5 class="card-title">Tomato</h5>
+               <p class="card-text">This</p>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="d-md-flex justify-content-md-end">
+         <a href="" class="btn btn-outline-dark" >See more...</a>
+      </div>
+   </section>
 
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
-              <div class="card h-100">
-                <img src="img/vegies.jpg" class="card-img-top" alt="vegies">
-                <div class="card-body">
-                  <h5 class="card-title">Vegetables</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, maxime!</p>
-                </div>            
-              </div>
+   <section id="categories" class="categories">
+      <a type="button" href="" class="d-grid btn-our" data-bs-toggle="button">Our Categories</a>
+
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+         <div class="col">
+            <div class="card h-100">
+               <img src="img/vegies.jpg" class="card-img-top" alt="vegies">
+               <div class="card-body">
+               <h5 class="card-title">Vegetables</h5>
+               <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, maxime!</p>
+               </div>            
             </div>
-            <div class="col">
-              <div class="card h-100">
-                <img src="img/meat.jpg" class="card-img-top" alt="meat">
-                <div class="card-body">
-                  <h5 class="card-title">Meat</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eum?</p>
-                </div>                
-              </div>
+         </div>
+         <div class="col">
+            <div class="card h-100">
+               <img src="img/meat.jpg" class="card-img-top" alt="meat">
+               <div class="card-body">
+               <h5 class="card-title">Meat</h5>
+               <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eum?</p>
+               </div>                
             </div>
-            <div class="col">
-              <div class="card h-100">
-                <img src="img/spices.jpg" class="card-img-top" alt="spices">
-                <div class="card-body">
-                  <h5 class="card-title">Spices</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, earum.</p>
-                </div>                
-              </div>
+         </div>
+         <div class="col">
+            <div class="card h-100">
+               <img src="img/spices.jpg" class="card-img-top" alt="spices">
+               <div class="card-body">
+               <h5 class="card-title">Spices</h5>
+               <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, earum.</p>
+               </div>                
             </div>
-          </div>
-    </section>
+         </div>
+         </div>
+   </section>
 </div>
 
 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -32,16 +32,16 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
          <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
             <li class="nav-item">
-               <a class="nav-link" href="#">Home</a>
+               <a class="nav-link" href="/home">Home</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">Features</a>
+               <a class="nav-link" href="#features">Features</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">Products</a>
+               <a class="nav-link" href="#product">Products</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">Categories</a>
+               <a class="nav-link" href="#categories">Categories</a>
             </li>         
          </ul>
 
@@ -51,7 +51,7 @@
             <button class="btn btn-outline-success" type="submit">Search</button>
          </form>
          <div class="icon ml-auto">            
-            <a href="#" class="logo mr-3"><i class="fa-solid fa-heart fa-lg"></i></a>
+            <a href="" class="logo mr-3"><i class="fa-solid fa-heart fa-lg"></i></a>
             <a href="#" class="logo mr-3"><i class="fa-solid fa-cart-shopping fa-lg"></i></a>
             <a href="#" class="logo mr-3"><i class="fa-solid fa-user fa-lg"></i></a>
          </div>
